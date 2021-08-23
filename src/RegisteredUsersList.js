e6f52f7630be9de44214271b12bd43ebd980c752
Healer404 <dukaroniel0701@gmail.com>
2021-08-23 12:47:41 +0800

    Added UI for the List of Registered Users
    Added a user interface for displaying the list of the registered users. The edit link per item is also added that can be redirected to the edit page with the specific id of the item selected.

--- src/RegisteredUsersList.js
+++ src/RegisteredUsersList.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 
 export const RegisteredUsersList = () => {
+    const [inputs, setInputs] = useState([]);
+
+    useEffect(() =>{
+        setInputs([
+            {
+                id: 1,
+                fname: "Roniel",
+                lname: "Duka",
+                mname: "Lucena",
+                age: 22,
+                sex: "male",
+                email: "dukaroniel0701@gmail.com",
+            },
+
+            {
+                id: 2,
+                fname: "Rae Julien",
+                lname: "Duka",
+                mname: "Lucena",
+                age: 15,
+                sex: "male",
+                email: "pukkaraj@gmail.com",
+            }
+        ])
+    }, []);
+
     return (
-        <div className="container p-5">
-            Registered users list
+        <div className="container">
+            <div className="mt-3">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>Registered Users List</h3>
+                    </div>
+                    <div className="card-body">
+                        <table className="table table-responsive table-hover">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                inputs.map(data => (
+                                    <tr key={data.id}>
+                                        <td>{data.id}</td>
+                                        <td>{data.lname}, {data.fname} {data.mname}</td>
+                                        <td>
+                                            <Link to={`/edit/${data.id}`}> Edit</Link>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
